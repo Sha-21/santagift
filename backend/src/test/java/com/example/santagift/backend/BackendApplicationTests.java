0820880d950978a5dfba10b11efaeb80382a2be9
shahin <shahin.biramane@gmail.com>
2021-12-14 10:57:17 +0100

    feat: Création du projet backend

---
+++ backend/src/test/java/com/example/santagift/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.santagift.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
